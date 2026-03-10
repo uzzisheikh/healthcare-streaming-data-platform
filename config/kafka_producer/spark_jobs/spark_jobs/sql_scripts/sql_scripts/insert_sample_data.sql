cef0e2f625ba629bb62a264b656c08eec6db68f6
CodeVector <uzzisheikh882@gmail.com>
2026-03-10 22:17:51 +0500

    Create insert_sample_data.sql

---
+++ config/kafka_producer/spark_jobs/spark_jobs/sql_scripts/sql_scripts/insert_sample_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO clinical_events (patient_id, event_type, timestamp) VALUES (1001, 'admission', 1678500000);
+INSERT INTO clinical_events (patient_id, event_type, timestamp) VALUES (1002, 'lab_result', 1678500600);
